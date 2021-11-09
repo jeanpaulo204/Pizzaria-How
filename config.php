ed03a6b2bb497b50d78cbaf7b5c0250a3c5a1f30
Jean Paulo <52867044+jeanpaulo204@users.noreply.github.com>
2021-11-09 13:30:24 -0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$dbHost = 'Localhost';
+$dbUsername = 'root';
+$dbPassword = '' ;
+$dbName = 'formulario';
+
+
+
+
+
+
+$conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+if($conexao->connect_errno)
+{
+  echo "Erro";
+}
+else
+{
+    echo "conexao realizada com sucesso";
+}
+
+
+?>
